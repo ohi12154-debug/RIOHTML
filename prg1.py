345ff48b3a205cb2966ba20be818c7b8f6f134d1
ohi12154-debug <ohi12154@gmail.com>
2026-06-04 21:51:56 +0600

    Add files via upload

---
+++ prg1.py
@@ -0,0 +1,8 @@
+number = int(input("enter number to check"))
+print("number to be check :", number)
+if number>0:
+    print("this is a positive number")
+elif number<0:
+    print("this is a negative number")
+else:
+    print("this is neutral")
