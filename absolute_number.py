ccab66088aaf54e42e90a031adb51a13b4deeb98
ohi12154-debug <ohi12154@gmail.com>
2026-06-11 21:43:50 +0600

    Add files via upload

---
+++ absolute_number.py
@@ -0,0 +1,7 @@
+def absolute_value(number):
+    if number >= 0:
+        return number
+    else:
+        return -number
+print("absolute value of 76 :", absolute_value(76))
+print("absolute value of -231 :", absolute_value(-231))
