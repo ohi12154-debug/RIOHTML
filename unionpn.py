b61b815f43cc4c0e7dd4db3b2cf643d1a6d91c90
ohi12154-debug <ohi12154@gmail.com>
2026-06-17 21:32:53 +0600

    Add files via upload

---
+++ unionpn.py
@@ -0,0 +1,24 @@
+class fruit:
+
+
+# Constructor
+
+    def __init__(self, n1, col):
+
+        self.name = n1
+        self.color = col
+
+# Object Creation
+ 
+ob1 = fruit('momo', 'steme')
+ob2 = fruit('ice-creame', 'chocolate')
+ob3 = fruit('ramen','3x spicy')
+
+# Calling Function
+
+print(ob1.name)
+print(ob1.color)
+print(ob2.name)
+print(ob2.color)
+print(ob3.name)
+print(ob3.color)
