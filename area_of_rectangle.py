aa59be9b1cc5edb30484e58e7e1ceb57ac928fe2
ohi12154-debug <ohi12154@gmail.com>
2026-06-18 21:18:30 +0600

    Add files via upload

---
+++ area_of_rectangle.py
@@ -0,0 +1,11 @@
+class Rectangle():
+    def __init__(self, l, w):
+        self.length = l
+        self.wedth = w
+    def Rectangle_area(self):
+        return self.lenght*self.wedth
+    
+    newrectangle = Rectangle(12, 10)
+print("dimension of Rectangle - length : %d width : %d" % (newRectangle.lengt
+    newRectangle.wedth))
+print("area of Rectangle :", newRectangle_area())
